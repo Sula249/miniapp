ccf6182733f5d888abecbd6fa5d81f95ae37f0a2
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-02-10 07:54:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ tg.expand();
 
 document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementById("searchButton");
-    const inputContainer = document.getElementById("inputContainer");
-    const inputField = document.getElementById("inputField");
-    const inputActionButton = document.getElementById("inputActionButton");
+    const searchContainer = document.getElementById("searchContainer");
+    const questionContainer = document.getElementById("questionContainer");
 
     searchButton.addEventListener("click", () => {
         searchButton.classList.add("flipped");
@@ -13,19 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
         setTimeout(() => {
             if (searchButton.innerText === "Начать поиск") {
                 searchButton.innerText = "Задать вопрос";
-                inputActionButton.innerText = "Поиск";
-                inputField.placeholder = "Введите запрос...";
+                searchContainer.classList.add("show");
+                questionContainer.classList.remove("show");
             } else {
                 searchButton.innerText = "Начать поиск";
-                inputActionButton.innerText = "Задать";
-                inputField.placeholder = "Введите вопрос...";
+                questionContainer.classList.add("show");
+                searchContainer.classList.remove("show");
             }
 
             searchButton.classList.remove("flipped");
-
-            // Переключаем видимость поля ввода
-            inputContainer.classList.toggle("show");
         }, 300);
     });
 });
- 
