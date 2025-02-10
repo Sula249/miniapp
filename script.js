df135c79d1ffde08b08d4bb169c05b1b44e7c902
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-02-10 06:06:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 let tg = window.Telegram.WebApp;
-tg.expand(); // Разворачивает приложение на весь экран
+tg.expand();
+
+const button = document.getElementById("toggleButton");
+
+button.addEventListener("click", () => {
+    if (button.textContent === "Начать поиск") {
+        button.textContent = "Задать вопрос";
+    } else {
+        button.textContent = "Начать поиск";
+    }
+});
