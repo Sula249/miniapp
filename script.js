bdaa5768243baf7b554742bd8f6455d2cfeb79aa
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-02-10 07:23:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementById("searchButton");
 
     searchButton.addEventListener("click", () => {
-        if (searchButton.innerText === "Начать поиск") {
-            searchButton.innerText = "Задать вопрос";
-        } else {
-            searchButton.innerText = "Начать поиск";
-        }
-        searchButton.classList.toggle("flipped");
+        searchButton.classList.add("flipped");
+
+        setTimeout(() => {
+            searchButton.innerText = searchButton.innerText === "Начать поиск" 
+                ? "Задать вопрос" 
+                : "Начать поиск";
+            searchButton.classList.remove("flipped");
+        }, 300); // Меняем текст после половины анимации (0.3s)
     });
 });
+
