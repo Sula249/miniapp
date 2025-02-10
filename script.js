0c2bb82f56b68f6ed4c31f40125606cc4f328642
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-02-10 06:21:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let tg = window.Telegram.WebApp;
 tg.expand(); // Разворачивает приложение на весь экран
  
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.getElementById("searchButton");
+
+    searchButton.addEventListener("click", () => {
+        if (searchButton.innerText === "Начать поиск") {
+            searchButton.innerText = "Задать вопрос";
+        } else {
+            searchButton.innerText = "Начать поиск";
+        }
+        searchButton.classList.toggle("flipped");
+    });
+});
