2d97d7e5816792fccd77d47ab29d6bf043eb81e9
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-02-11 22:33:15 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    OPENROUTER_API_KEY: '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',
+    GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycbypBtYb0Y8XiGYlEpRyzJq_yqCrE5ieiFwXT92MPsSF29EIFQLmOcp0gZZXasgQb3S9/exec',
+    GOOGLE_CSE_ID: 'e58c2408882ae476e'
+}; 
